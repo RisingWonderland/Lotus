ceffe61d610789eee5166dae7812e1006d3c318f
RisingWonderland <risingwonderland@gmail.com>
2015-06-17 10:19:36 +0800

    Clear up

--- src/org/rw/crow/example/FileTest.java
+++ src/org/rw/crow/example/FileTest.java
@@ -1,4 +1,9 @@
 package org.rw.crow.example;
+
+import java.io.File;
+
+import org.rw.crow.io.FileUtils;
+
 /**
  * Test FileUtils
  * @author Crow
@@ -8,7 +13,13 @@ package org.rw.crow.example;
 public class FileTest {
 
 	public static void main(String[] args) {
+//		FileUtils.iterateDir(new File("G:\\TFS Basic User Guide"));
+//		File logFile = new File("E:\\logForLotus.txt");
+//		FileUtils.write(logFile, System.currentTimeMillis(), true);
 		
+		File fileShouldBeRead = new File("G:\\TempFiles\\小知识.txt");
+		String data = FileUtils.readEntrieFile(fileShouldBeRead);
+		System.out.println(data);
 	}
 	
 }
--- src/org/rw/crow/io/FileUtils.java
+++ src/org/rw/crow/io/FileUtils.java
@@ -1,4 +1,16 @@
 package org.rw.crow.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.rw.crow.regular.CheckValid;
+
 /**
  * Provides some methods for manipulate files.
  * 
@@ -13,5 +25,106 @@ public class FileUtils {
 	private FileUtils(){}
 	
 	
+	/**
+	 * Traverse files and directories within the target file.
+	 * @author Crow
+	 * @date 2015年5月25日
+	 * @version v0.1
+	 * @param dirFile the directory we iterated
+	 */
+	public static void iterateDir(File dirFile){
+		File[] files = dirFile.listFiles();
+		for(File file : files){
+			if(file.isFile()){
+				System.out.println(file.getAbsolutePath().toString());
+			}else{
+				iterateDir(file);
+			}
+		}
+	}
+	
+	/**
+	 * Writes string content to append to a file.
+	 * @author Crow
+	 * @date 2015年5月25日
+	 * @version v0.1
+	 * @param file the file we want to write
+	 * @param content the content we wrote
+	 */
+	public static void write(File file, Object content){
+		write(file, content, true);
+	}
+	
+	/**
+	 * Writes string content into a file.
+	 * @author Crow
+	 * @date 2015年5月25日
+	 * @version v0.1
+	 * @param file the file we want to write
+	 * @param content the content we wrote
+	 */
+	public static void write(File file, Object content, boolean append){
+		FileOutputStream fos = null;
+		try {
+			 fos = new FileOutputStream(file, append);
+			 byte[] data = content.toString().getBytes();
+			 fos.write(data);
+			 fos.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fos != null){
+					fos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Read all the contents of a text file.
+	 * @author Crow
+	 * @date 2015年5月25日
+	 * @version v0.1
+	 * @param file
+	 * @return
+	 */
+	public static String readEntrieFile(File file){
+		CheckValid.checkNotDocument(file);
+		if(!CheckValid.checkIsDocument(file)) return "";
+		
+		StringBuilder sbd = new StringBuilder();
+		InputStream is = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		try {
+			is = new FileInputStream(file);
+			isr = new InputStreamReader(is, "UTF-8");
+			br = new BufferedReader(isr);
+			char[] data = new char[1024];
+			int len = 0;
+			while((len = br.read(data)) != -1){
+				sbd.append(String.valueOf(data, 0, len));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(is != null){
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return sbd.toString();
+	}
 	
 }
